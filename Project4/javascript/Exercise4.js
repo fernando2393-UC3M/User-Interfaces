a498e48eda4adfc2d41ddb930d2ac32ac0d379cf
fernando2393 <garciasanzfernando2393@gmail.com>
2017-10-21 13:41:46 +0200

    Photo Uploader

--- Project4/javascript/Exercise4.js
+++ Project4/javascript/Exercise4.js
@@ -1,3 +1,22 @@
+/*Photo Uploader*/
+
+function previewFile(){
+       var preview = document.querySelector(".photo"); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
+
 /*Cookies Block*/
 
 function setCookie(cname, cvalue, exdays) {
@@ -46,6 +65,17 @@ function checkCookiePass() {
   }
 }
 
+function checkCookieUsrName() {
+  var usr = getCookie("usr");
+  if (usr != "") {
+  } else {
+    usr = document.getElementById("Username").value;
+    if (usr != "" && usr != null) {
+      setCookie("usr", usr, 30);
+    }
+  }
+}
+
 function checkCookieFName() {
   var fname = getCookie("fname");
   if (fname != "") {
@@ -53,6 +83,28 @@ function checkCookieFName() {
     fname = document.getElementById("FirstName").value;
     if (fname != "" && fname != null) {
       setCookie("fname", fname, 30);
+    }
+  }
+}
+
+function checkCookieSurname() {
+  var surname = getCookie("surname");
+  if (surname != "") {
+  } else {
+    surname = document.getElementById("Surname").value;
+    if (surname != "" && surname != null) {
+      setCookie("surname", surname, 30);
+    }
+  }
+}
+
+function checkCookieLanguage() {
+  var lang = getCookie("lang");
+  if (lang != "") {
+  } else {
+    surname = document.getElementById("Language").value;
+    if (lang != "" && lang != null) {
+      setCookie("lang", lang, 30);
     }
   }
 }
@@ -66,7 +118,10 @@ function setMail(){
 }
 
 function setFName(){
-  document.getElementById("FirstName").value = getCookie("fname");
+  var fname = getCookie("fname");
+  if(fname != "" && fname != null){
+    document.getElementById("FirstName").value = getCookie("fname");
+  }
 }
 
 function resetAllCookies(){
