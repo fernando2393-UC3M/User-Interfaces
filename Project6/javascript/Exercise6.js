5fbdb40f4c16e8cf6323061491b5954e781194b0
JavaLavadora <tonidiazmartinez1997@gmail.com>
2017-10-25 18:35:53 +0200

    Update Exercise6.js

--- Project6/javascript/Exercise6.js
+++ Project6/javascript/Exercise6.js
@@ -1,42 +1,121 @@
-//Global varibales
-var n = 0;
-var chrono = 0;
-var counter = 0;
-var x;
-var game;
+var Source = "#boxcard";
+var ImgSource = [
+  "images/samsung.jpg",
+  "images/lg.jpg",
+  "images/onplus.jpg",
+  "images/apple.jpg",
+  "images/huawei.jpg",
+  "images/oppo.jpg",
+  "images/vivo.jpg",
+  "images/xiaomi.jpg",
+  "images/zte.jpg",
+  "images/htc.jpg",
+];
+var BoxOpened = ""; //Holds id of 2º image openned
+var ImgOpened = ""; //Holds id of 1º image openned
+var clickNum = 0;
+var ImgFound = 0;
 
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+
+$(document).ready(function(){
+  function modalBox() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+  }
+
+  function modalBoxHide() {
+    if (checkImages() === true && checkTime() === true) {
+      var modal = document.getElementById('myModal');
+      modal.style.display = "none";
     }
   }
-  return "";
+
+  for (var y = 1; y < 3 ; y++) {  //Appends the images twice (one div per image)
+  	$.each(ImgSource, function(i, val) {
+  		$(Source).append("<div id=card" + y + i + "><img src=" + val + "/>");
+  	});
+  }
+	$(Source + " div").click(OpenCard); //Image clicked
+	ShuffleImages();
+});
+
+function OpenCard() {
+	var id = $(this).attr("id"); //Id of openned card
+	if ($("#"+id+" img").is(":hidden")) { //Only on hidden images
+		$("#" + id + " img").slideDown('fast'); //Animation
+
+		if (ImgOpened == ""){
+			BoxOpened = id;
+			ImgOpened = $("#" + id + " img").attr("src"); //Get source of 1º openned img
+		}
+    else{
+		    CurrentOpened = $("#" + id + " img").attr("src"); //Get source of 2º openned img
+
+			  if (ImgOpened != CurrentOpened) {//Images do NOT match
+  			     setTimeout(function() {
+					     $("#" + id + " img").slideUp('fast');
+				       $("#" + BoxOpened + " img").slideUp('fast');
+        		   BoxOpened = "";
+        		   ImgOpened = "";
+				     },400);
+			   }
+         else {//Images MATCH
+    				$("#" + id + " img").parent().css("visibility", "hidden");  //Delete images already paired
+    				$("#" + BoxOpened + " img").parent().css("visibility", "hidden");
+            ImgFound++;
+            checkWin();
+    				BoxOpened = "";
+    				ImgOpened = ""; //Reset the selected images
+			   }
+		}
+
+		clickNum++;
+		$("#clickNum").html("" + clickNum); //Display updated clickNum
+
+	}
 }
 
-function modalBox() {
-  var modal = document.getElementById('myModal');
-  modal.style.display = "block";
+function ShuffleImages() {
+	var ImgAll = $(Source).children();
+	var ImgThis = $(Source + " div:first-child");
+	var ImgArr = new Array(); //Contains the src of the images
+
+	for (var i = 0; i < ImgAll.length; i++) { //Gets src of each image and stores them in ImgArr
+		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
+		ImgThis = ImgThis.next();
+	}
+
+	ImgThis = $(Source + " div:first-child"); //We go back to first child
+
+	for (var z = 0; z < ImgAll.length; z++) { //Gives a random value
+	  var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]); //Changes the src to a random one coming from ImgArr
+		ImgArr.splice(RandomNumber, 1);  //Removes 1 item located at RandomNumber-->Removes the src which have already been assigned
+		ImgThis = ImgThis.next();
+	}
 }
 
-function modalBoxHide() {
-  if (checkImages() === true && checkTime() === true) {
-    var modal = document.getElementById('myModal');
-    modal.style.display = "none";
+function RandomFunction(MaxValue, MinValue) {
+		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+}
+
+function ResetGame() {
+	ShuffleImages();
+	$(Source + " div img").hide(); //Hide image???
+	$(Source + " div").css("visibility", "visible"); //Set the div to visible
+	clickNum = 0;
+	$("#success").remove(); //Removes success
+	$("#clickNum").html("" + clickNum); //Display clickNum as 0
+	BoxOpened = "";
+	ImgOpened = "";
+	ImgFound = 0;
+	return false;
+}
+
+function checkWin(){
+  if (ImgFound == ImgSource.length) { //Check if user has won
+    alert("WINNER WINNER CHICKEN DINER");
   }
 }
 
@@ -50,14 +129,6 @@ function checkImages() {
   if (imagesn < 3 || imagesn > 10) {
     alert("The number must be between 3 and 10")
     return false;
-  }
-  var cimage = getCookie("cimage");
-  if (cimage === "") {
-    setCookie("cimage", images, 30);
-  }
-  if (cimage != "") {
-    document.cookie = "cimage=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    setCookie("cimage", images, 30);
   }
   n = imagesn * 2;
   return true;
@@ -74,246 +145,6 @@ function checkTime() {
     alert("The time must be between 10 and 120")
     return false;
   }
-  var ctimer = getCookie("ctimer");
-  if (ctimer === "") {
-    setCookie("ctimer", time, 30);
-  }
-  if (ctimer != "") {
-    document.cookie = "ctimer=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    setCookie("ctimer", time, 30);
-  }
   chrono = timen;
   return true;
 }
-
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
-}
-
-function timer() {
-  counter = chrono;
-  var min = 0;
-  var sec = 0;
-  if (counter > 0) {
-     x = setInterval(function() {
-      if (counter > 60) {
-        min = 1;
-        sec = counter - 60;
-        document.getElementById("Timer").innerHTML = min + " minute and " + sec + " seconds";
-        counter--;
-      }
-      if (counter === 60){
-         min = 1;
-        document.getElementById("Timer").innerHTML = min + " minute";
-        counter--;
-      }
-      if (counter < 60) {
-        sec = counter;
-        document.getElementById("Timer").innerHTML = sec + " seconds";
-        counter--;
-      }
-      if (counter < 1){
-        document.getElementById("Timer").innerHTML = "EXPIRED!";
-        alert("Time has expired!");
-      }
-    }, 1000);
-  }
-}
-
-function hidePlay() {
-  document.getElementById("Play").style.visibility = "hidden";
-  document.getElementById("Restart").style.visibility = "visible";
-}
-
-function hideRestart() {
-  document.getElementById("Restart").style.visibility = "hidden";
-  document.getElementById("Play").style.visibility = "visible";
-}
-
-function hideTimer() {
-  document.getElementById("Timer").style.visibility = "hidden";
-}
-
-function showTimer() {
-  document.getElementById("Timer").style.visibility = "visible";
-}
-
-function restart(){
-  clearInterval(x);
-  //startGame();
-}
-
-//------------------------------------------------------------------------------
-
-
-
-(function(){
-
-	var Memory = {
-
-		init: function(cards){
-			this.$game = $(".game");
-			this.$restartButton = $("button.restart");
-			this.cardsArray = $.merge(cards, cards);
-			this.shuffleCards(this.cardsArray);
-			this.setup();
-			this.binding();
-		},
-
-		shuffleCards: function(cardsArray){
-			this.$cards = $(this.shuffle(this.cardsArray));
-		},
-
-		setup: function(){
-			this.html = this.buildHTML();
-			this.$game.html(this.html);
-			this.$memoryCards = $(".card");
-			this.paused = false;
-     	this.guess = null;
-		},
-
-		binding: function(){
-			this.$memoryCards.on("click", this.cardClicked);
-			this.$restartButton.on("click", $.proxy(this.reset, this));
-		},
-
-		cardClicked: function(){
-			var _ = Memory;
-			var $card = $(this);
-			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
-				$card.find(".inside").addClass("picked");
-				if(!_.guess){
-					_.guess = $(this).attr("data-id");
-				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
-					$(".picked").addClass("matched");
-					_.guess = null;
-				} else {
-					_.guess = null;
-					_.paused = true;
-					setTimeout(function(){
-						$(".picked").removeClass("picked");
-						Memory.paused = false;
-					}, 600);
-				}
-				if($(".matched").length == $(".card").length){
-					_.win();
-				}
-			}
-		},
-
-		win: function(){
-			this.paused = true;
-			setTimeout(function(){
-				Memory.$game.fadeOut();
-			}, 1000);
-		},
-
-		reset: function(){
-			this.shuffleCards(this.cardsArray);
-			this.setup();
-			this.$game.show("slow");
-		},
-
-		shuffle: function(array){
-			var counter = array.length, temp, index;
-	   	// While there are elements in the array
-	   	while (counter > 0) {
-        	// Pick a random index
-        	index = Math.floor(Math.random() * counter);
-        	// Decrease counter by 1
-        	counter--;
-        	// And swap the last element with it
-        	temp = array[counter];
-        	array[counter] = array[index];
-        	array[index] = temp;
-	    	}
-	    	return array;
-		},
-
-		buildHTML: function(){
-			var frag = '';
-			this.$cards.each(function(k, v){
-				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
-				<div class="front"><img src="'+ v.img +'"\
-				alt="'+ v.name +'" /></div>\
-				<div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
-				alt="Codepen" /></div></div>\
-				</div>';
-			});
-			return frag;
-		}
-	};
-
-	var cards = [
-		{
-			name: "php",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
-			id: 1,
-		},
-		{
-			name: "css3",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
-			id: 2
-		},
-		{
-			name: "html5",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
-			id: 3
-		},
-		{
-			name: "jquery",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
-			id: 4
-		},
-		{
-			name: "javascript",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
-			id: 5
-		},
-		{
-			name: "node",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
-			id: 6
-		},
-		{
-			name: "photoshop",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
-			id: 7
-		},
-		{
-			name: "python",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
-			id: 8
-		},
-		{
-			name: "rails",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
-			id: 9
-		},
-		{
-			name: "sass",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
-			id: 10
-		},
-		{
-			name: "sublime",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
-			id: 11
-		},
-		{
-			name: "wordpress",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
-			id: 12
-		},
-	];
-
-	Memory.init(cards);
-
-
-})();
