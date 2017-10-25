db4d7902224d05c4298ecc41214b535cf6751667
fernando2393 <garciasanzfernando2393@gmail.com>
2017-10-26 01:44:07 +0200

    Changes

--- Project6/javascript/Exercise6.js
+++ Project6/javascript/Exercise6.js
@@ -16,111 +16,118 @@ var ImgOpened = ""; //Holds id of 1º image openned
 var clickNum = 0;
 var ImgFound = 0;
 var numImg;
+var counter;
 
 var x; //For the timer
 var chrono = 0;
 
 
-function start(){
-  numImg= subArray();
-  for (var y = 1; y < 3 ; y++) {  //Appends the images twice (one div per image)
-  	numImg.map(function(val, i) {
-  		$(Source).append("<div id=card" + y + i + "><img class='srcImg' src=" + val + "/>");
-  	});
+function start() {
+  numImg = subArray();
+  for (var y = 1; y < 3; y++) { //Appends the images twice (one div per image)
+    numImg.map(function(val, i) {
+      $(Source).append("<div id=card" + y + i + "><img class='srcImg' src=" + val + "/>");
+    });
   }
-	$(Source + " div").click(OpenCard); //Image clicked
-	ShuffleImages();
+  $(Source + " div").click(OpenCard); //Image clicked
+  ShuffleImages();
 }
 
-function subArray(){
-  var num=$("#Images").val();
-  var subar=[];
-  for(var i=0;i<num;i++){
-    subar[i]=ImgSource[i];
+function subArray() {
+  var num = $("#Images").val();
+  var subar = [];
+  for (var i = 0; i < num; i++) {
+    subar[i] = ImgSource[i];
   }
   // var subar=ImgSource.slice(1, num);
   return subar;
 }
 
 function OpenCard() {
-	var id = $(this).attr("id"); //Id of openned card
-	if ($("#"+id+" img").is(":hidden")) { //Only on hidden images
-		$("#" + id + " img").slideDown('fast'); //Animation
+  var id = $(this).attr("id"); //Id of openned card
+  if ($("#" + id + " img").is(":hidden")) { //Only on hidden images
+    $("#" + id + " img").slideDown('fast'); //Animation
+    $(document).ready(function() {
+      $("#picbox").click(function() {
+        $(this).addClass('flip');
+      });
+    });
 
-		if (ImgOpened == ""){
-			BoxOpened = id;
-			ImgOpened = $("#" + id + " img").attr("src"); //Get source of 1º openned img
-		}
-    else{
-		    CurrentOpened = $("#" + id + " img").attr("src"); //Get source of 2º openned img
+    if (ImgOpened == "") {
+      BoxOpened = id;
+      ImgOpened = $("#" + id + " img").attr("src"); //Get source of 1º openned img
+    } else {
+      CurrentOpened = $("#" + id + " img").attr("src"); //Get source of 2º openned img
 
-			  if (ImgOpened != CurrentOpened) {//Images do NOT match
-  			     setTimeout(function() {
-					     $("#" + id + " img").slideUp('fast');
-				       $("#" + BoxOpened + " img").slideUp('fast');
-        		   BoxOpened = "";
-        		   ImgOpened = "";
-				     },400);
-			   }
-         else {//Images MATCH
-    				$("#" + id + " img").parent().css("visibility", "visible");  //Delete images already paired
-    				$("#" + BoxOpened + " img").parent().css("visibility", "visible");
-            ImgFound++;
-            checkWin();
-    				BoxOpened = "";
-    				ImgOpened = ""; //Reset the selected images
-			   }
-		}
+      if (ImgOpened != CurrentOpened) { //Images do NOT match
+        setTimeout(function() {
+          $("#" + id + " img").slideUp('fast');
+          $("#" + BoxOpened + " img").slideUp('fast');
+          BoxOpened = "";
+          ImgOpened = "";
+        }, 400);
+      } else { //Images MATCH
+        $("#" + id + " img").parent().css("visibility", "visible"); //Delete images already paired
+        $("#" + BoxOpened + " img").parent().css("visibility", "visible");
+        ImgFound++;
+        checkWin();
+        BoxOpened = "";
+        ImgOpened = ""; //Reset the selected images
+      }
+    }
 
-		clickNum++;
-		$("#clickNum").html("" + clickNum); //Display updated clickNum
+    clickNum++;
+    $("#clickNum").html("" + clickNum); //Display updated clickNum
 
-	}
+  }
 }
 
 function ShuffleImages() {
-	var ImgAll = $(Source).children();
-	var ImgThis = $(Source + " div:first-child");
-	var ImgArr = new Array(); //Contains the src of the images
+  var ImgAll = $(Source).children();
+  var ImgThis = $(Source + " div:first-child");
+  var ImgArr = new Array(); //Contains the src of the images
 
-	for (var i = 0; i < ImgAll.length; i++) { //Gets src of each image and stores them in ImgArr
-		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
-		ImgThis = ImgThis.next();
-	}
+  for (var i = 0; i < ImgAll.length; i++) { //Gets src of each image and stores them in ImgArr
+    ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
+    ImgThis = ImgThis.next();
+  }
 
-	ImgThis = $(Source + " div:first-child"); //We go back to first child
+  ImgThis = $(Source + " div:first-child"); //We go back to first child
 
-	for (var z = 0; z < ImgAll.length; z++) { //Gives a random value
-	  var RandomNumber = RandomFunction(0, ImgArr.length - 1);
-		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]); //Changes the src to a random one coming from ImgArr
-		ImgArr.splice(RandomNumber, 1);  //Removes 1 item located at RandomNumber-->Removes the src which have already been assigned
-		ImgThis = ImgThis.next();
-	}
+  for (var z = 0; z < ImgAll.length; z++) { //Gives a random value
+    var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+    $("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]); //Changes the src to a random one coming from ImgArr
+    ImgArr.splice(RandomNumber, 1); //Removes 1 item located at RandomNumber-->Removes the src which have already been assigned
+    ImgThis = ImgThis.next();
+  }
 }
 
 function RandomFunction(MaxValue, MinValue) {
-		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+  return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
 }
 
 function ResetGame() {
-	ShuffleImages();
-	$(Source + " div img").hide(); //Hide image???
-	$(Source + " div").css("visibility", "visible"); //Set the div to visible
-	clickNum = 0;
-	$("#success").remove(); //Removes success
-	$("#clickNum").html("" + clickNum); //Display clickNum as 0
-	BoxOpened = "";
-	ImgOpened = "";
-	ImgFound = 0;
-	return false;
+  ShuffleImages();
+  $(Source + " div img").hide(); //Hide image???
+  $(Source + " div").css("visibility", "visible"); //Set the div to visible
+  clickNum = 0;
+  $("#success").remove(); //Removes success
+  $("#clickNum").html("" + clickNum); //Display clickNum as 0
+  BoxOpened = "";
+  ImgOpened = "";
+  ImgFound = 0;
+  return false;
 }
 
-function checkWin(){
-  BoxOpened = "";//Reset the selected images (in order to avoid malfunctioning)
+function checkWin() {
+  BoxOpened = ""; //Reset the selected images (in order to avoid malfunctioning)
   ImgOpened = "";
-  alert(ImgFound);
   if (ImgFound == numImg.length) { //Check if user has won
+    document.getElementById("Timer").innerHTML = "Congratulations";
     alert("WINNER WINNER CHICKEN DINER");
+    alert("The game has lasted "+(chrono-counter-1)+" seconds");
+    clearInterval(x);
+
   }
 }
 
@@ -146,11 +153,14 @@ function checkImages() {
   }
   var imagesn = parseInt(document.getElementById('Images').value);
   if (imagesn < 3 || imagesn > 10) {
-    alert("The number must be between 3 and 10")
+    alert("The number must be between 3 and 10");
+    location.reload();
     return false;
   }
-  n = imagesn * 2;
-  return true;
+  if(imagesn > 2 || imagesn < 11){
+    n = imagesn * 2;
+    return true;
+  }
 }
 
 function checkTime() {
@@ -174,15 +184,15 @@ function timer() {
   var min = 0;
   var sec = 0;
   if (counter > 0) {
-     x = setInterval(function() {
+    x = setInterval(function() {
       if (counter > 60) {
         min = 1;
         sec = counter - 60;
         document.getElementById("Timer").innerHTML = min + " minute and " + sec + " seconds";
         counter--;
       }
-      if (counter === 60){
-         min = 1;
+      if (counter === 60) {
+        min = 1;
         document.getElementById("Timer").innerHTML = min + " minute";
         counter--;
       }
@@ -191,9 +201,11 @@ function timer() {
         document.getElementById("Timer").innerHTML = sec + " seconds";
         counter--;
       }
-      if (counter < 1){
+      if (counter < 0) {
         document.getElementById("Timer").innerHTML = "EXPIRED!";
         alert("Time has expired!");
+        clearInterval(x);
+        location.reload();
       }
     }, 1000);
   }
