76ac6bf91f88f5ee73ff40b14d6fb2c1256795e9
fernando2393 <garciasanzfernando2393@gmail.com>
2017-11-18 13:25:28 +0100

    Final Project

---
+++ Final Project/Javascript/javascript.js
@@ -0,0 +1,33 @@
+function checkPass() {
+
+  /* parámetros que debe cumplir,
+  Minimo 8 caracteres
+  Maximo 15
+  Al menos una letra mayúscula
+  Al menos una letra minucula
+  Al menos un dígito
+  No espacios en blanco
+  Al menos 1 caracter especial
+  */
+
+  var regexp_password = /^(?=.*[a-z])(?=.*\d)([A-Za-z\d$@$!%*?&]|[^ ]){1,8}$/;
+  var regexp_email = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  // cojo el valor de los input
+  var contrasenia = document.getElementById('passwordModal').value;
+  var email = document.getElementById('emailModal').value;
+  cemail = "Email";
+  cpass = "Pass";
+  // compruebo si contrasenia cumple las condiciones de expresión regular, lo hago mediante función test.
+  if (regexp_password.test(contrasenia) && regexp_email.test(email)) {
+    document.getElementById('myModal').style.display = "none";
+    return true;
+  } else {
+    if (!regexp_password.test(contrasenia)) {
+      alert('Wrong...! Please put a correct password!');
+      return false;
+    } else {
+      alert('Wrong...! Please put a correct email!');
+      return false;
+    }
+  }
+}
---
+++ Final Project/readme.md
@@ -0,0 +1,7 @@
+This is the final project of User Interfaces subject.
+
+Job performed by:
+
+Fernando García Sanz
+Antonio de Padua Díaz Martínez
+Sergio Cruzado Muñoz
